d94197ba7c9e62152a9365124a190b89e572387e
trungbui23102003 <103498612+trungbui23102003@users.noreply.github.com>
2022-06-02 18:45:20 +0700

    Javascript

--- assets/main.js
+++ assets/main.js
@@ -288,3 +288,262 @@ console.log(
     age: 16,
   })
 );
+
+// singly linked list
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+//
+for (var i = 100; i >= 0; i -= 5) {
+  console.log(i);
+}
+
+////
+function giaiThua(num){
+  var output = 1;
+  for(var i = num; i > 0 ; i--){
+    output = output * i;
+  }
+  return output;
+}
+console.log(giaiThua(4))
+
+
+// 11.113: Thực hành sử dụng phương thức reduce #2
+var watchList = [
+  {
+    "Title": "Inception",
+    "Year": "2010",
+    "Rated": "PG-13",
+    "Released": "16 Jul 2010",
+    "Runtime": "148 min",
+    "Genre": "Action, Adventure, Crime",
+    "Director": "Christopher Nolan",
+    "Writer": "Christopher Nolan",
+    "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+    "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+    "Language": "English, Japanese, French",
+    "Country": "USA, UK",
+    "imdbRating": "8.8",
+    "imdbVotes": "1,446,708",
+    "imdbID": "tt1375666",
+    "Type": "movie",
+  },
+  {
+    "Title": "Interstellar",
+    "Year": "2014",
+    "Rated": "PG-13",
+    "Released": "07 Nov 2014",
+    "Runtime": "169 min",
+    "Genre": "Adventure, Drama, Sci-Fi",
+    "Director": "Christopher Nolan",
+    "Writer": "Jonathan Nolan, Christopher Nolan",
+    "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+    "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    "Language": "English",
+    "Country": "USA, UK",
+    "imdbRating": "8.6",
+    "imdbVotes": "910,366",
+    "imdbID": "tt0816692",
+    "Type": "movie",
+  },
+  {
+    "Title": "The Dark Knight",
+    "Year": "2008",
+    "Rated": "PG-13",
+    "Released": "18 Jul 2008",
+    "Runtime": "152 min",
+    "Genre": "Action, Adventure, Crime",
+    "Director": "Christopher Nolan",
+    "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+    "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+    "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+    "Language": "English, Mandarin",
+    "Country": "USA, UK",
+    "imdbRating": "9.0",
+    "imdbVotes": "1,652,832",
+    "imdbID": "tt0468569",
+    "Type": "movie",
+  },
+  {
+    "Title": "Batman Begins",
+    "Year": "2005",
+    "Rated": "PG-13",
+    "Released": "15 Jun 2005",
+    "Runtime": "140 min",
+    "Genre": "Action, Adventure",
+    "Director": "Christopher Nolan",
+    "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+    "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+    "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+    "Language": "English, Urdu, Mandarin",
+    "Country": "USA, UK",
+    "imdbRating": "8.3",
+    "imdbVotes": "972,584",
+    "imdbID": "tt0372784",
+    "Type": "movie",
+  },
+  {
+    "Title": "Avatar",
+    "Year": "2009",
+    "Rated": "PG-13",
+    "Released": "18 Dec 2009",
+    "Runtime": "162 min",
+    "Genre": "Action, Adventure, Fantasy",
+    "Director": "James Cameron",
+    "Writer": "James Cameron",
+    "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+    "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+    "Language": "English, Spanish",
+    "Country": "USA, UK",
+    "imdbRating": "7.9",
+    "imdbVotes": "876,575",
+    "imdbID": "tt0499549",
+    "Type": "movie",
+  }
+];
+
+function calculateRating(arr){
+  var a = arr.filter(function(movie){
+    return movie.Director === 'Christopher Nolan'
+  })
+  var b = a.reduce(function(total, imdb){
+    return total + Number(imdb.imdbRating);
+  }, 0)
+  console.log(b / a.length);
+  return b / a.length;
+}
+
+
+// Expected results
+console.log(calculateRating(watchList)); // Output: 8.675
+
+// 11.111 : thực hành sử dụng phương thức reduce #1
+var sports = [
+  {
+      name: 'Bơi lội',
+      gold: 11
+  },
+  {
+      name: 'Boxing',
+      gold: 3
+  },
+  {
+      name: 'Đạp xe',
+      gold: 4
+  },
+  {
+      name: 'Đấu kiếm',
+      gold: 5
+  },
+]
+
+function getTotalGold(data){
+  return data.reduce(function(total, element){
+      return total + element.gold
+  }, 0)
+}
+
+
+
+// Expected results:
+console.log(getTotalGold(sports)) // Output: 23
+
+
+// 11.115: Thực hành sử dụng phương thức reduce #3
+var arr = [['name', 'Sơn Đặng'], ['age', 18]]
+   function arrToObj(arr){
+    return  arr.reduce(function(a,b){
+        return Object.fromEntries(arr);
+    },
+    {})
+}
+// Expected results
+console.log(arrToObj(arr)) // Output: { name: 'Sơn Đặng', age: 18 }
+
+
+
+// 12.119: Tạo chương trình tính toán
+function sumCb(a, b) {
+  return a + b;
+}
+
+function subCb(a, b){
+  return a - b;
+}
+
+function multiCb(a, b){
+  return a * b;
+}
+
+function divCb(a, b){
+  return a / b;
+}
+
+function caculate(a, b, cb) {
+  return cb(a, b);
+}
+
+// Expected results
+caculate(1, 2, sumCb) // Output: 3
+caculate(1, 2, subCb) // Output: -1
+caculate(1, 2, multiCb) // Output: 2
+caculate(3, 1, divCb) // Output: 3
+
+
+
+// 12.121 Thực hành tạo phuong thức myMap() :
+Array.prototype.myMap = function(cb) {
+  var out = [], n= this.length;
+  for(var i =0; i < n; i++){
+      var results = cb(this[i],i)
+      out.push(results)
+  }
+  return out;
+}
+
+// Expected results
+// const numbers = [1, 2, 3];
+
+// console.log(numbers.myMap(function (number) {
+//     return number * 2;
+// })) // Output: [2, 4, 6]
+
+// console.log(numbers.myMap(function (number, index) {
+//     return number * index;
+// })) // Output: [0, 2, 6]
+
+
+Array.prototype.mySome = function (cb) {
+  for (var index in this) {
+      if (this.hasOwnProperty(index)) {
+          var result = cb(this[index], index, this)
+          if (result) {
+              return true;
+          } 
+      }
+  }
+  return false;
+}
+
+
+console.log("CCCCCCCCCCCCCCCCCCCCCCCC");
+
+const listF = [
+  () => {
+    console.log("F0");
+  },
+  () => {
+    console.log("F1");
+  },
+  () => {
+    listF[0]();
+    listF[1]();
+  }
+];
+
+listF[2]();
